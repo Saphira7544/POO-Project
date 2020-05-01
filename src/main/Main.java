bb4800a15e2ff1a3d387783d0909a28fbb61426f
Saphira7544 <isabel.pcastelo@gmail.com>
2020-05-01 18:19:57 +0000

    main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		// Exits if arguments are not correctly input
+		if(args.length != 3) { 
+			System.err.println("Necessary arguments: TrainFile TestFile Score(LL or MDL)");
+			System.exit(-1);
+		}
+		
+		File TrainFile = new File(args[0]);
+		try {
+			TrainSet TrainData = new TrainSet(TrainFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
-
+6tgb
 }
