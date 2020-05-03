c7b0bd215fcb328a08f47513a885eb25f1450292
Tiago <tiago17silverio1@gmail.com>
2020-05-03 18:03:10 +0100

    a

--- src/main/Graph.java
+++ src/main/Graph.java
@@ -71,5 +71,89 @@ private void updateNodeCountsFromPattern(TrainSet set, Pattern newPattern) {
 
 
 	
+<<<<<<< Updated upstream
+=======
+	@Override
+	public String toString() {
+		String listS = new String("Graph \n");
+			
+		for (Node N: DAG.keySet()){
+			listS += N.toString() + "=" + DAG.get(N).toString() + "\n";
+		} 
+		
+		return listS;
+	}
+	
+
+public void updateNodeCounts(TrainSet set)  {
+	
+	Node node1 = new Node("X1", 2);
+	
+	int nrInstances = 5; //get from isabel
+
+	int nrXs = DAG.size(); //get from isabel
+
+	// Next lines will initialize the variable Node.counts of every Node in this.nodeArray
+	
+	int nrclass = 2; //get from isabel
+	// Initialize nodes' counts
+	
+	for (int i = 0; i < nrXs; i++) {
+		// for each parent node i
+		node1.Nijkc = new int[nrXs][][][];
+		
+		for (int j = 0; j < nrclass; j++) {
+			
+			// iniciate all possible values of Nijkc for each son
+			g.get(i).Nijkc[j] = new int[range de j][range de i][classRange];
+		}
+		DAG.get(i).Nijc = new int[range de i][classRange];
+	}
+	DAG.Nc = new int[classRange];
+
+	// Now the listOfTrainPatterns will be iterated and Node.counts will be updated
+	Iterator<Instance> iterator = set.getInstance().iterator();
+
+	Pattern newPattern;
+
+	while (iterator.hasNext()) {
+		newInstance = iterator.next();
+
+		updateNodeCountsFromPattern(set, newInstance);
+	}
+
+}
+
+private void updateNodeCountsFromPattern(TrainSet set, Pattern newPattern) {
+
+	int nrXs = DAG.size();
+
+	outputNode.cCounts[newPattern.getOutput()]++;
+
+	for (int i = 0; i < nrXs; i++) { // each Xi
+		for (int p = 0; p < nrXs; p++) { // each possible parent node
+
+			if (p == i) { // case in which Xi has no parent. We will store this case in the position where node Xi is the parent of itself
+
+				/*
+				 * For Node Xi, increment counts' entry that corresponds to: empty parent configuration, node Xi has value pattern[i] and
+				 * classNode has value output
+				 */
+				DAG.get(i).Nijkc[i][0][ valor de Xi][valor de C]++;
+				continue;
+			}
+			/*
+			 * For Node Xi, increment counts' entry that corresponds to: parent p has value pattern[p], node Xi has value pattern[i] and classNode
+			 * has value output
+			 */
+			DAG.get(i).Nijkc[p][ valor de Xj ][ valor de Xi ][valor de C ]++;
+		}
+
+		DAG.get(i).Nijc[ valor de Xi ][valor de C]++;
+	}
+}
+
+
+>>>>>>> Stashed changes
 }
 
--- src/main/Node.java
+++ src/main/Node.java
@@ -2,4 +2,54 @@ package main;
 
 public class Node {
 
+<<<<<<< Updated upstream
+=======
+public class Node{
+	
+	private final String label;	
+	private final int range;
+	int[][][][] Nijkc;
+	
+	
+	private int[][] Nijc;
+	private int[] cCounts;
+	
+
+	public Node(String key, int range){
+		this.label = key;
+		this.range = range;
+	}
+
+	public String getKey() {
+		return label;
+	}
+	
+	public int getRange() {
+		return range;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		return Objects.equals(label, other.label) & Objects.equals(range, other.range);
+	}
+		
+	@Override
+	public String toString() {
+		return label;
+	}
+	
+	
+
+	
+	
+	
+	
+>>>>>>> Stashed changes
 }
